ffcbeec2889d61e7b2aff84719bafa9936892598
dv-joan <joan300501@gmail.com>
2023-05-10 14:33:43 -0500

    feat: Created & Customized

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import uncpLogo from "./assets/images/uncp-logo.png";
+import fisLogo from "./assets/images/fis-logo.png";
+import Fetch from "./components/Fetch";
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<div className="App m-10">
+			<header className="App-header flex justify-between items-center">
+				<img src={uncpLogo} width={100} className="uncp-logo" alt="logo" />
+				<div className="text-center space-y-2">
+					<strong className="text-3xl">
+						Universidad Nacional Del Centro del Perú
+					</strong>
+					<h4 className="text-2xl">Facultad de Ingenieria de Sistemas</h4>
+				</div>
+				<img src={fisLogo} width={100} className="fis-logo" alt="logo" />
+			</header>
+			<div className="flex justify-center">
+				<Fetch />
+			</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Fetch.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import Products from "../data/products.json";
+import { Table } from "@mantine/core";
+
+function Fetch() {
+	const [products, setProducts] = useState(Products);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
+	if (Array.isArray(products)) {
+		const rows = products.map((product) => (
+			<tr key={product.id}>
+				<td>{product.nombre}</td>
+				<td>{product.descripcion}</td>
+				<td>{product.precio}</td>
+				<td>
+					<img
+						src={product.imagen}
+						width={20}
+						height={20}
+						alt="product-image"
+					/>
+				</td>
+			</tr>
+		));
+	} else {
+		console.warn("No se pudo cargar la data");
+	}
+	useEffect(() => {
+		setTimeout(() => {
+			setLoading(false);
+			setError(false);
+		}, 1000);
+	}, []);
+	return (
+		<div className="">
+			<h1 className="font-bold text-2xl">Fetch</h1>
+			{loading && <p>Loading...</p>}
+			{error && <p>Error :(</p>}
+			<Table horizontalSpacing="md" verticalSpacing="xl" fontSize="xs">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Description</th>
+						<th>Price</th>
+						<th>Image</th>
+					</tr>
+				</thead>
+				<tbody>{rows}</tbody>
+			</Table>
+		</div>
+	);
+}
+
+export default Fetch;
