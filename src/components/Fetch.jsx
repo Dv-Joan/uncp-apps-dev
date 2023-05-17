e05e1f5bf8158af13f9b707de56ad72657ec816f
dv-joan <joan300501@gmail.com>
2023-05-16 20:46:28 -0500

    fix: Fetch ERROR Fixed

--- src/components/Fetch.jsx
+++ src/components/Fetch.jsx
@@ -1,73 +1,60 @@
-import React, { useState, useEffect } from "react";
-import Products from "../data/products.json";
+import { useState, useEffect } from "react";
+import Products from "./../data/products.json"
 import { Table } from "@mantine/core";
 
 function Fetch() {
-	const [products, setProducts] = useState(Products);
+	const [products, setProducts] = useState([]);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(false);
-	if (Array.isArray(products)) {
-		const rows = products.map((product) => (
-			<tr key={product.id}>
-				<td>{product.nombre}</td>
-				<td>{product.descripcion}</td>
-				<td>{product.precio}</td>
-				<td>
-					<img
-						src={product.imagen}
-						width={20}
-						height={20}
-						alt="product-image"
-					/>
-				</td>
-			</tr>
-		));
-	} else {
-		console.warn("No se pudo cargar la data");
-	}
+	const url = "https://fakestoreapi.com/products";
+
+	const handleFetch = async () => {
+		try {
+			const response = await fetch(url);
+			const data = await response.json();
+			setProducts(data);
+			console.log(data);
+		} catch (error) {
+			setError(true);
+		} finally {
+			setLoading(false);
+		}
+	};
+
 	useEffect(() => {
 		setTimeout(() => {
 			setLoading(false);
 			setError(false);
 		}, 1000);
+		handleFetch();
 	}, []);
 	return (
 		<div className="">
 			<h1 className="font-bold text-2xl">Fetch</h1>
 			{loading && <p>Loading...</p>}
 			{error && <p>Error :(</p>}
-			<Table horizontalSpacing="md" verticalSpacing="xl" fontSize="xs">
+			<Table horizontalSpacing="md" className="text-neutral-200" verticalSpacing="md" fontSize="sm">
 				<thead>
 					<tr>
-						<th>Name</th>
+						<th>Title</th>
 						<th>Description</th>
 						<th>Price</th>
 						<th>Image</th>
 					</tr>
 				</thead>
 				<tbody>
-					{Array.isArray(products) ? (
+					{
 						products.map((product) => (
 							<tr key={product.id}>
-								<td>{product.nombre}</td>
-								<td>{product.descripcion}</td>
-								<td>{product.precio}</td>
+								<td >{product.title}</td>
+								<td className="w-64">{product.description}</td>
+								{/* <td className="w-24">$ {product.price}</td> */}
 								<td>
-									<img
-										src={product.imagen}
-										width={20}
-										height={20}
-										alt="product-image"
-									/>
+									<img src={product.image} width={50} height={50} alt={product.title} />
 								</td>
 							</tr>
 						))
-					) : (
-							<tr>
-								<td colSpan="4">No hay productos</td>
-						</tr>
-					)
-						
+
 					}
 				</tbody>
 			</Table>
