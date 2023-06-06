ca3a11cd927a82b1d8d40f84829f4e0316eedf90
dv-joan <joan300501@gmail.com>
2023-06-06 14:27:02 -0500

    feat: Week nine added

--- src/components/second-partial.jsx
+++ src/components/second-partial.jsx
@@ -5,7 +5,7 @@ import { WeeklyButton, Goals } from './ui/elements'
 function SecondPartial() {
     return (
         <div className='flex justify-center h-auto gap-5 mt-5'>
-            <WeeklyButton dropDownposition="" weekNumber={1} weeklyGoals={<Goals weeklyGoalOne="Participación activa" weeklyGoalTwo="Comprensión del silabus" weeklyGoalThree="Formación de Grupos de Trabajo" weeklyClassDescription="Se realizó la exposicion del silabus del curso de desarrollo de aplicaciones web para el ciclo 2023-I con los diferentes temas que se tratarán durante el perido académico" />} weeklyProject={<Routing />} />
+            <WeeklyButton dropDownposition="right" weekNumber={9} weeklyGoals={<Goals weeklyGoalOne="Participación activa" weeklyGoalTwo="Comprensión del silabus" weeklyGoalThree="Formación de Grupos de Trabajo" weeklyClassDescription="Se realizó la exposicion del silabus del curso de desarrollo de aplicaciones web para el ciclo 2023-I con los diferentes temas que se tratarán durante el perido académico" />} weeklyProject={<Routing />} />
         </div>
 
     )
